8736c17a184edb10b3cb2f5a46b2b064ee8d5794
wvfx-windro <100778740+wvfx-windro@users.noreply.github.com>
2022-03-08 23:19:21 +0530

    Update StringSession.js

--- Amru/StringSession.js
+++ Amru/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Sana:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Amru:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
