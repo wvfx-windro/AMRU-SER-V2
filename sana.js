bc2fef7964036262716f0c7c3649eea9b78f9045
wvfx-windro <100778740+wvfx-windro@users.noreply.github.com>
2022-03-08 23:18:07 +0530

    Update sana.js

--- sana.js
+++ sana.js
@@ -8,7 +8,7 @@ WhatsAsena - Yusuf Usta
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./Sana/');
+const {StringSession} = require('./Amru/');
 const fs = require('fs');
 
 async function whatsAsena () {
