be40c73241306a0eb12e9e21a56da7ba434c1854
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-03-08 10:21:26 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,33 +1,41 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-
+const Config = require('../config');
+const fs = require('fs');
+const got = require('got');
+const config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: 'command help menu'}, (async (message, match) => {
 
     var r_text = new Array ();
     
-    
-    r_text[0] = "https://i.ibb.co/Y2mXdR5/ajuser.jpg";
-    
+    r_text[0] = Config.LG_LOGO
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `𝙰𝙱𝚄 𝚂𝙴𝚁 𝙱𝙾𝚃
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.jpg, contextInfo: {mentionedJid: message.mention}, caption: `
+   ` + config.LOGO_NAME + `
 
-*bot making video* : 
+*Oᴡɴᴇʀ Nᴜᴍʙᴇʀ* :https://wa.me/` + config.PHONE + `*            
 
-*owner id instagram* :
 
-*github* : 
+*Wʜᴀᴛsᴀᴘᴘ Gʀᴏᴜᴘ* :` + config.GROUP + `*
 
-■□■□■□■□■□■□■□■□■□■□
-       _𝑨𝑩𝑼 𝑺𝑬𝑹 𝑩𝑶𝑻_
-  ▣▣ 𝙲𝚁𝙴𝙰𝚃𝙴𝙳 𝙱𝚈 𝙰𝙱𝚄 𝚂𝙴𝚁 ▣▣
+
+*Gɪᴛ Lɪɴᴋs* : https://github.com/Afx-Abu/Abu_ser*
+
+
+*Aᴜᴅɪᴏ ᴄᴏᴍᴍᴀɴᴅs* :https://github.com/Afx-Abu/Abu-media/tree/main/bgm*
+
+
+*sᴛɪᴄᴋᴇʀ ᴄᴏᴍᴍᴀɴᴅs* :https://github.com/Afx-Abu/Abu-media/tree/main/stickers*
+
+*ᴛʜɪs ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ` + config.BOT + `*
 
 `}) 
 
